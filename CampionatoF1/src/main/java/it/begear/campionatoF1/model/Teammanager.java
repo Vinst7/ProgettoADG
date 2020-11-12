d082319bfcc3f065fd3115c9f0e759232a02360b
Vinst7 <vincenzo.stingo7@gmail.com>
2020-11-12 17:40:32 +0100

    pushVin

---
+++ CampionatoF1/src/main/java/it/begear/campionatoF1/model/Teammanager.java
@@ -0,0 +1,101 @@
+package it.begear.campionatoF1.model;
+
+public class Teammanager {
+	private int id;
+	private String nome;
+	private String cognome;
+	private int annoNascita;
+	private String nazionalita;
+	private int durataContratto;
+	private int stipendioAnnuo;
+	private int idScuderia;
+	
+	public Teammanager() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Teammanager(int id, String nome, String cognome, int annoNascita, String nazionalita, int durataContratto,
+			int stipendioAnnuo, int idScuderia) {
+		this.id = id;
+		this.nome = nome;
+		this.cognome = cognome;
+		this.annoNascita = annoNascita;
+		this.nazionalita = nazionalita;
+		this.durataContratto = durataContratto;
+		this.stipendioAnnuo = stipendioAnnuo;
+		this.idScuderia = idScuderia;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCognome() {
+		return cognome;
+	}
+
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+
+	public int getAnnoNascita() {
+		return annoNascita;
+	}
+
+	public void setAnnoNascita(int annoNascita) {
+		this.annoNascita = annoNascita;
+	}
+
+	public String getNazionalita() {
+		return nazionalita;
+	}
+
+	public void setNazionalita(String nazionalita) {
+		this.nazionalita = nazionalita;
+	}
+
+	public int getDurataContratto() {
+		return durataContratto;
+	}
+
+	public void setDurataContratto(int durataContratto) {
+		this.durataContratto = durataContratto;
+	}
+
+	public int getStipendioAnnuo() {
+		return stipendioAnnuo;
+	}
+
+	public void setStipendioAnnuo(int stipendioAnnuo) {
+		this.stipendioAnnuo = stipendioAnnuo;
+	}
+
+	public int getIdScuderia() {
+		return idScuderia;
+	}
+
+	public void setIdScuderia(int idScuderia) {
+		this.idScuderia = idScuderia;
+	}
+
+	@Override
+	public String toString() {
+		return "Ingegneri [id=" + id + ", nome=" + nome + ", cognome=" + cognome + ", annoNascita=" + annoNascita
+				+ ", nazionalita=" + nazionalita + ", durataContratto=" + durataContratto + ", stipendioAnnuo="
+				+ stipendioAnnuo + ", idScuderia=" + idScuderia + "]";
+	}
+	
+
+}
